5a46fae5e6f109e752d646c5df710f20300d81e1
라브소소 <nelpsos@gmail.com>
2026-05-21 09:19:11 +0900

    카드 프레임 및 뒷면 색상 수정

--- SomniaCottonwoodCode/Character/SomniaCottonwoodCardPool.cs
+++ SomniaCottonwoodCode/Character/SomniaCottonwoodCardPool.cs
@@ -6,20 +6,22 @@ namespace SomniaCottonwood.SomniaCottonwoodCode.Character;
 
 public class SomniaCottonwoodCardPool : CustomCardPoolModel
 {
-    public override string Title => SomniaCottonwood.CharacterId; //This is not a display name.
-    
+    public override string Title => SomniaCottonwood.CharacterId;
+    public override string EnergyColorName => SomniaCottonwood.CharacterId;
+
     public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
     public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+
 
 
     /* These HSV values will determine the color of your card back.
     They are applied as a shader onto an already colored image,
     so it may take some experimentation to find a color you like.
     Generally they should be values between 0 and 1. */
-    public override float H => 1f; //Hue; changes the color.
-    public override float S => 1f; //Saturation
-    public override float V => 1f; //Brightness
-    
+    public override float H => 0.244f; //Hue; changes the color.
+    public override float S => 0.24f; //Saturation
+    public override float V => 0.43f; //Brightness
+
     //Alternatively, leave these values at 1 and provide a custom frame image.
     /*public override Texture2D CustomFrame(CustomCardModel card)
     {
@@ -28,7 +30,9 @@ public class SomniaCottonwoodCardPool : CustomCardPoolModel
     }*/
 
     //Color of small card icons
-    public override Color DeckEntryCardColor => new("ffffff");
-    
+    public override Color DeckEntryCardColor => new("616d53");
+
+    public override Color EnergyOutlineColor => new("1A6625");
+
     public override bool IsColorless => false;
 }
